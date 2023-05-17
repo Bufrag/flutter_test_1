645435ebeb60958afb99d5a685e95ebe909aa424
Bufrag <bufrag.wow@gmail.com>
2023-05-17 22:56:05 +0800

    add new library
    добавил еще одну библиотеку

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MaterialApp());
