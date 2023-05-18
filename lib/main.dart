94a745b7cc0844971144af353b387f46948fbe0d
Bufrag <bufrag.wow@gmail.com>
2023-05-18 20:58:19 +0800

    delete library
    Удалил библиотеку

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MaterialApp());
