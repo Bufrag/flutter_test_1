cbce30dc0df3f647ec6c61b12abf9c65c8f95bb5
Bufrag <bufrag.wow@gmail.com>
2023-05-18 22:59:48 +0800

    Delete cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/scheduler.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
 
 void main() {
   runApp(const MaterialApp());
