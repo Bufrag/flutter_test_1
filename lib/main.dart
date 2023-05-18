8fcc7c13e452527aaa70e7e9ad7c8c915ba8ce8f
Bufrag <bufrag.wow@gmail.com>
2023-05-18 22:51:27 +0800

    Put back
    вернул назад

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/scheduler.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 void main() {
   runApp(const MaterialApp());
